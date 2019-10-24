58a602b20055319ceb4aa031c64cd1f434b38803
Mathias Elken <math079e@edu.eal.dk>
2019-10-24 13:13:52 +0200

    7imuh5n4brg4t

---
+++ Trendlog - Gruppe 2/Models/Channel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trendlog___Gruppe_2.Models
+{
+    public class Channel
+    {
+        public int ChannelId { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public string Timezone { get; set; }
+        public int Lifetime { get; set; }
+        public int Longitume { get; set; }
+        public int Elevation { get; set; }
+        public string Email { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public int MaxSize { get; set; }
+        public int PCount { get; set; }
+        public List<Feed> FeedList { get; set; }
+
+
+
+    }
+}
---
+++ Trendlog - Gruppe 2/Models/Feed.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trendlog___Gruppe_2.Models
+{
+    public class Feed
+    {
+        public int FeedId { get; set; }
+        public string Name { get; set; }
+        public string Label { get; set; }
+        public int Scale { get; set; }
+        public int Alarm { get; set; }
+        public Boolean EnableAlarm { get; set; }
+        public string AlarmSettings { get; set; }
+        public List<Point> Points { get; set; }
+    }
+}
